462c218c1d3cfbc612fee8852a766d0be9ab9c36
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-16 22:00:46 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,38 @@
 def consolidate_cart(cart)
-  # code here
-end
+  cart_hash = {}
+   cart.each do |item|
+     item.each do |name, attribute|
+       if cart_hash.has_key?(name)
+         cart_hash[name][:count] += 1
+       else
+         cart_hash = cart_hash.merge({name => attribute.merge({count: 1})})
+       end
+     end
+   end
+   return cart_hash
+ end
 
 def apply_coupons(cart, coupons)
-  # code here
+  cart_hash = cart
+  
+  coupons.each do |coupon|
+    item_name = coupon[:item]
+    if cart_hash.keys.include?(item_name)
+      cart_count = cart_hash[item_name][:count]
+      
+      if cart_count >= coupon[:num]
+        item_coup = {"#{item_name} W/COUPON" => {
+          price: coupon[:cost], 
+          clearance: cart_hash[item_name][:clearance], 
+          count: cart_count/coupon[:num]}
+        }
+        
+        cart_hash[item_name][:count] %= coupon[:num]
+        cart_hash = cart_hash.merge(item_coup)
+      end
+    end
+  end
+  return cart_cons
 end
 
 def apply_clearance(cart)
